4fa3df5f70c8b52fa26ed7d603db1b37329a5fb6
sablin <sablin.irk@gmail.com>
2017-09-07 17:12:37 +0800

    Использование промиса для вывода пользователей
    Начало приложения

--- lesson_1/app/js/common.js
+++ lesson_1/app/js/common.js
@@ -1,8 +1,30 @@
-$.ajax({
-        url: 'https://randomuser.me/api/?inc=name,email,picture',
-        dataType: 'json',
-        success: function(data) {
-           console.log(data);
-        }
-    });
-
+function sendAjax(url){
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.responseType = 'json';
+        xhr.addEventListener('load',()=>{
+            resolve(xhr.response);
+        });
+        xhr.addEventListener('error', () =>{
+            reject();
+        })
+        xhr.send();
+    })
+}
+let but = document.getElementById('getUser');
+but.addEventListener('click',()=>{
+    sendAjax('https://randomuser.me/api/?inc=name,email,dob,picture')
+        .then((response)=>{
+            let img = new Image();
+            let date = new Date(response.results[0].dob).getFullYear();
+            img.src = response.results[0].picture.medium;
+            userWindow.appendChild(img);
+            let div = document.createElement('div');
+            let info = response.results[0].name.first + ' | ' + date + ' | ' + response.results[0].email;
+            userWindow.appendChild(div).innerText = info;
+            },
+        ()=>{
+            alert('Ќе удалось получить данные с сервера');
+        });
+})
--- lesson_1/app/js/es6/common.js
+++ lesson_1/app/js/es6/common.js
@@ -1,9 +1,30 @@
 'use strict';
 
-$.ajax({
-   url: 'https://randomuser.me/api/?inc=name,email,picture',
-   dataType: 'json',
-   success: function success(data) {
-      console.log(data);
-   }
+function sendAjax(url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.responseType = 'json';
+        xhr.addEventListener('load', function () {
+            resolve(xhr.response);
+        });
+        xhr.addEventListener('error', function () {
+            reject();
+        });
+        xhr.send();
+    });
+}
+var but = document.getElementById('getUser');
+but.addEventListener('click', function () {
+    sendAjax('https://randomuser.me/api/?inc=name,email,dob,picture').then(function (response) {
+        var img = new Image();
+        var date = new Date(response.results[0].dob).getFullYear();
+        img.src = response.results[0].picture.medium;
+        userWindow.appendChild(img);
+        var div = document.createElement('div');
+        var info = response.results[0].name.first + ' | ' + date + ' | ' + response.results[0].email;
+        userWindow.appendChild(div).innerText = info;
+    }, function () {
+        alert('�� ������� �������� ������ � �������');
+    });
 });
